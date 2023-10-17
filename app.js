cfe964410d733ad5aae30a47ddf27d0d376ab225
Hannah Nguyen <hnguyen160801@gmail.com>
2023-10-17 02:00:31 -0400

    add dynamodb

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 
-const bookRoutes = require("./routes/books")
+const bookRoutes = require("./routes/books");
+const userRoutes = require("./routes/users");
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
   // res.send("hello world!")
-  res.json({"greeting": "Hello world!"})
-})
+  res.json({ "greeting": "Hello world!" });
+});
 
-app.use("/books", bookRoutes)
+app.use("/books", bookRoutes);
+app.use("/users", userRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
---
+++ dynamo.js
@@ -0,0 +1,100 @@
+"use strict"
+
+const AWS = require('aws-sdk');
+require('dotenv').config();
+
+AWS.config.update({
+  region: process.env.DEFAULT_REGION,
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY
+});
+
+const dynamoClient = new AWS.DynamoDB.DocumentClient();
+const TABLE_NAME = "reading-app";
+
+const getUsers = async () => {
+  try {
+    const params = {
+      TableName: TABLE_NAME
+    };
+    const users = await dynamoClient.scan(params).promise();
+    console.log(users);
+    return users;
+  } catch (error) {
+    console.error("Error retrieving users: ", error);
+    throw error; // Optionally re-throw the error for the calling code to handle.
+  }
+};
+
+const getUserById = async (id) => {
+  try {
+    const params = {
+      TableName: TABLE_NAME,
+      Key: {
+        id
+      }
+    };
+    return await dynamoClient.get(params).promise();
+  } catch (error) {
+    console.error("Error getting user by id: ", error);
+  }
+};
+
+const addOrUpdateUser = async (user) => {
+  try {
+    const params = {
+      TableName: TABLE_NAME,
+      Item: user
+    };
+
+    return await dynamoClient.put(params).promise();
+
+  } catch (error) {
+    console.error("Error updating or creating user: ", error);
+  }
+};
+
+const deleteUser = async (id) => {
+  try {
+    const params = {
+      TableName: TABLE_NAME,
+      Key: {
+        id
+      }
+    };
+    return await dynamoClient.delete(params).promise();
+  } catch (error) {
+    console.error("Error deleting user: ", error);
+  }
+};
+
+// addOrUpdateUser({
+//   id: "0",
+//   email: "email@email.com",
+//   password: "password",
+//   name: "hannah",
+//   age: 22,
+//   books: {
+//     1342: 40,
+//     37106: 3
+//   }
+// })c\
+
+// (async () => {
+//   const user = await getUserById("0");
+//   if (user.Item) {
+//     console.log("User found:", user.Item);
+//   } else {
+//     console.log("User not found.");
+//   }
+// })();
+
+// getUsers();
+
+module.exports = {
+  dynamoClient,
+  getUsers,
+  getUserById,
+  addOrUpdateUser,
+  deleteUser
+};
